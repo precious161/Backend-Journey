2c5e71ef0d9a71faf77a4de9ea4ff04afecf8d84
Feyruza Dawud <feyruza.dawud-ug@aau.edu.et>
2026-04-11 15:08:35 +0300

    feat(auth): implement JWT infrastructure and modular auth hooks

--- week3-tasks-api/src/app.ts
+++ week3-tasks-api/src/app.ts
@@ -1,8 +1,11 @@
 import Fastify from "fastify";
 import prismaPlugin from "./plugins/prisma.js";
 import { TaskRoutes } from "./routes/tasks.routes.js";
+import  authPlugin from "./plugins/auth.js";
 
 export const app = Fastify({logger:true});
 
 app.register(prismaPlugin);
+app.register(authPlugin);
 app.register(TaskRoutes,{prefix:'/tasks'});
+
